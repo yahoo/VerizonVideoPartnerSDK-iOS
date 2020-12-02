6b0301284c15f41ddd27f9296886e65a22d271ee
Yi-Chin Sun <yichin.sun@gmail.com>
2020-12-02 18:24:40 -0500

    Add host app for tests

---
+++ VerizonVideoPartnerSDKHost/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VerizonVideoPartnerSDKHost
+//
+//  Created by Yi-Chin Sun on 12/2/20.
+//  Copyright © 2020 Oath Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ VerizonVideoPartnerSDKHost/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VerizonVideoPartnerSDKHost
+//
+//  Created by Yi-Chin Sun on 12/2/20.
+//  Copyright © 2020 Oath Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
